277138039b0c325559d15fe21e855bd54507a29c
rushati <rushati.modak@gmail.com>
2026-05-12 12:05:55 +0530

    function to change uppercase to lowercase

--- one.py
+++ one.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+def Hello(n):
+    return n.lower()
+
+
+def main():
+    a = input("Enter in uppercase: ")
+    whisper = Hello(a)
+    print(f"Speak quietly -- {whisper}")
+
+if __name__ == "__main__":
+    main()
